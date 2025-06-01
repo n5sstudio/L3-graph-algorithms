b79c65cb2d311860f2f568a404012a88fb26c812
n5sstudio <n5sstudio@outlook.com>
2025-06-01 11:41:23 +0200

    feat: extract main function in JUnit tests

--- src/main/java/com/n5sstudio/Graph.java
+++ src/main/java/com/n5sstudio/Graph.java
@@ -1,4 +1,4 @@
-package main.java.com.n5sstudio;
+package com.n5sstudio;
 
 public class Graph {
 
@@ -6,7 +6,7 @@ public class Graph {
     private int maximumNumberOfVertex;
     private boolean[] vertexExistanceArray;
     private static int UNDEFINED = 0;
-    
+
     public Graph() {
         maximumNumberOfVertex = 1000;
         adjacencyMatrix = new int[maximumNumberOfVertex][maximumNumberOfVertex];
@@ -18,7 +18,7 @@ public class Graph {
             }
         }
     }
-    
+
     public Graph(int[][] adjacencyMatrix) {
         maximumNumberOfVertex = 2 * adjacencyMatrix.length;
         adjacencyMatrix = new int[maximumNumberOfVertex][maximumNumberOfVertex];
@@ -39,7 +39,6 @@ public class Graph {
         }
     }
 
-    // CONSTRUCTEUR GRAPHE ; PARAM. NB MAXI DE SOMMETS.
     public Graph(int maximumNumberOfVertex) {
         this.maximumNumberOfVertex = maximumNumberOfVertex;
         adjacencyMatrix = new int[maximumNumberOfVertex][maximumNumberOfVertex];
@@ -52,7 +51,6 @@ public class Graph {
         }
     }
 
-    // CONSTRUCTEUR GRAPHE CLONE ; PARAM. GRAPHE.
     public Graph(Graph graph) {
         maximumNumberOfVertex = graph.maximumNumberOfVertex;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -60,12 +58,14 @@ public class Graph {
         }
     }
 
-    // EXISTANCE D'UN SOMMET.
+    public int getUndefiledValue() {
+        return Graph.UNDEFINED;
+    }
+
     public boolean hasVertex(int vertexIndex) {
         return vertexExistanceArray[vertexIndex];
     }
 
-    // NOMBRE DE SOMMETS
     public int getVertexCount() {
         int cpt = 0;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -75,7 +75,6 @@ public class Graph {
         return cpt;
     }
 
-    // AJOUTER UN SOMMET.
     public boolean addVertex(int vertexIndex) {
         if (vertexIndex < maximumNumberOfVertex && !hasVertex(vertexIndex)) {
             vertexExistanceArray[vertexIndex] = true;
@@ -85,7 +84,6 @@ public class Graph {
         }
     }
 
-    // SUPPRIMER UN SOMMET.
     public boolean deleteVertex(int vertexIndex) {
         if (!hasVertex(vertexIndex)) {
             return false;
@@ -99,16 +97,15 @@ public class Graph {
         }
     }
 
-    // EXISTANCE D'UN ARC.
     public boolean hasArc(int originVertexIndex, int destinationVertexIndex) {
-        if (hasVertex(originVertexIndex) && hasVertex(destinationVertexIndex) && adjacencyMatrix[originVertexIndex][destinationVertexIndex] != UNDEFINED) {
+        if (hasVertex(originVertexIndex) && hasVertex(destinationVertexIndex)
+                && adjacencyMatrix[originVertexIndex][destinationVertexIndex] != UNDEFINED) {
             return true;
         } else {
             return false;
         }
     }
 
-    // VALEUR D'UN ARC.
     public int getArcValue(int originVertexIndex, int destinationVertexIndex) {
         if (hasArc(originVertexIndex, destinationVertexIndex)) {
             return adjacencyMatrix[originVertexIndex][destinationVertexIndex];
@@ -117,9 +114,9 @@ public class Graph {
         }
     }
 
-    // AJOUTER UN ARC.
     public boolean addArc(int originVertexIndex, int destinationVertexIndex, int val) {
-        if (hasVertex(originVertexIndex) && hasVertex(destinationVertexIndex) && adjacencyMatrix[originVertexIndex][destinationVertexIndex] == UNDEFINED) {
+        if (hasVertex(originVertexIndex) && hasVertex(destinationVertexIndex)
+                && adjacencyMatrix[originVertexIndex][destinationVertexIndex] == UNDEFINED) {
             adjacencyMatrix[originVertexIndex][destinationVertexIndex] = val;
             return true;
         } else {
@@ -127,7 +124,6 @@ public class Graph {
         }
     }
 
-    // SUPPRIMER UN ARC.
     public boolean deleteArc(int originVertexIndex, int destinationVertexIndex) {
         if (hasArc(originVertexIndex, destinationVertexIndex)) {
             adjacencyMatrix[originVertexIndex][destinationVertexIndex] = UNDEFINED;
@@ -137,7 +133,6 @@ public class Graph {
         }
     }
 
-    // DEGRE SORTANT
     public int getVertexOutDegree(int vertexIndex) {
         int degE = 0;
         for (int j = 0; j < maximumNumberOfVertex; j++) {
@@ -147,7 +142,6 @@ public class Graph {
         return degE;
     }
 
-    // DEGRE ENTRANT
     public int getVertexInDegree(int vertexIndex) {
         int degS = 0;
         for (int j = 0; j < maximumNumberOfVertex; j++) {
@@ -157,12 +151,10 @@ public class Graph {
         return degS;
     }
 
-    // DEGRE
     public int getVertexDegree(int vertexIndex) {
         return getVertexInDegree(vertexIndex) + getVertexOutDegree(vertexIndex);
     }
 
-    // LISTE DES SUCCESSEURS.
     public int[] getSuccessorList(int vertexIndex) {
         int[] liste = new int[maximumNumberOfVertex];
         int k = 0;
@@ -177,7 +169,6 @@ public class Graph {
         return liste;
     }
 
-    // LISTE DES PREDECESSEURS
     public int[] getPredecessorList(int vertexIndex) {
         int[] liste = new int[maximumNumberOfVertex];
         int k = 0;
@@ -192,24 +183,6 @@ public class Graph {
         return liste;
     }
 
-    // toString
-    public java.lang.String toString() {
-        String ch = "";
-        int[] lst_sommet;
-        for (int i = 0; i < maximumNumberOfVertex; i++) {
-            if (hasVertex(i)) {
-                lst_sommet = getSuccessorList(i);
-                ch = ch + i + " :";
-                for (int j = 0; j < getVertexOutDegree(i); j++) {
-                    ch = ch + lst_sommet[j];
-                }
-                ch = ch + "\n";
-            }
-        }
-        return ch;
-    }
-
-    // R�flexivit�
     public boolean isReflexive() {
         boolean Refl = true;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -222,7 +195,6 @@ public class Graph {
         return Refl;
     }
 
-    // Anti-R�flexivit�
     public boolean isIrreflexive() {
         boolean ARefl = true;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -235,7 +207,6 @@ public class Graph {
         return ARefl;
     }
 
-    // Sym�trie
     public boolean isSymmetric() {
         boolean Sym = true;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -250,7 +221,6 @@ public class Graph {
         return Sym;
     }
 
-    // Anti-Sym�trie
     public boolean isAntisymmetric() {
         boolean ASym = true;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -265,7 +235,6 @@ public class Graph {
         return ASym;
     }
 
-    // Transitivit�
     public boolean isTransitive() {
         boolean Trans = true;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -282,7 +251,6 @@ public class Graph {
         return Trans;
     }
 
-    // Anti-Transitivit�
     public boolean isAntiTransitive() {
         boolean ATrans = true;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -299,7 +267,6 @@ public class Graph {
         return ATrans;
     }
 
-    // Transposition
     public void transpose() {
         int tmp;
         for (int i = 0; i < maximumNumberOfVertex; i++) {
@@ -311,56 +278,30 @@ public class Graph {
         }
     }
 
-    // Fin des Methodes
-
-    // Fonction MAIN
-    // On fait ici une s�rie de test permettant de v�rifier si le programme est
-    // coh�rent
-    public static void main(String[] args) {
-        Graph g = new Graph(10); // On cr�e le graphe g pouvant disposer de 10 sommets au maximum
-        System.out.println(g.hasVertex(2)); // FALSE, en effet le sommet 2 n'existe pas.
-        System.out.println(g.addVertex(1)); // TRUE, le sommet 1 vient d'�tre cr�e
-        System.out.println(g.addVertex(2)); // TRUE, le sommet 2 vient d'�tre cr�e
-        System.out.println(g.addVertex(3)); // TRUE, le sommet 3 vient d'�tre cr�e
-        System.out.println(g.hasVertex(1)); // TRUE, en effet le sommet 1 existe
-        System.out.println(g.getVertexCount()); // 3, il y a bien 3 sommets dans le Graphe g
-        System.out.println(g.hasArc(1, 3)); // FALSE, en effet l'arc n'existe pas
-        System.out.println(g.getArcValue(1, 2)); // 0, 0 correspond � ALPHA_NOTDEF, l'arc n'existe pas
-        System.out.println(g.addArc(1, 3, 5)); // TRUE, l'arc 1-3 vient d'�tre cr�e et a pour valeur 5
-        System.out.println(g.addArc(1, 2, 3)); // TRUE, l'arc 1-2 vient d'�tre cr�e et a pour valeur 3
-        System.out.println(g.getArcValue(1, 2)); // 3, l'arc 1-3 a pour valeur 3
-        System.out.println(g.getArcValue(3, 1)); // 0, l'arc 3-1 n'existe pas !
-        System.out.println(g.addVertex(4)); // TRUE, le sommet 4 vient d'�tre cr�e
-        System.out.println(g.hasVertex(4)); // TRUE, en effet le sommet 4 existe
-        System.out.println(g.deleteVertex(4)); // TRUE, le sommet 4 est supprim�
-        System.out.println(g.hasVertex(4)); // FALSE, en effet le sommet 4 n'existe plus
-        System.out.println(g.getVertexInDegree(1)); // 0 qui est bien le degr�s entrant de 1
-        System.out.println(g.getVertexInDegree(2)); // 1 qui est bien le degr�s entrant de 2
-        System.out.println(g.addArc(3, 1, 3)); // TRUE, l'arc 3-1 vient d'�tre cr�e et a pour valeur 3
-        System.out.println(g.addArc(2, 3, 3)); // TRUE, l'arc 2-3 vient d'�tre cr�e et a pour valeur 3
-        System.out.println(g.getVertexOutDegree(1)); // 2, qui est bien le dergr�s sortant de 1
-        System.out.println(g.getVertexOutDegree(3)); // 1, qui est bien le dergr�s sortant de 3
-        System.out.println(g.getVertexDegree(1)); // 3, qui est bien le dergr�s de 1
-        System.out.println(g.getVertexDegree(3)); // 3, qui est bien le dergr�s de 3
-        System.out.println(g.toString()); // 1 :23 ; 2 :3 ; 3 :1 (probl�me d'�criture s'il y a plusieurs sommets !!!)
-        System.out.println(g.isReflexive()); // FALSE OK
-        System.out.println(g.isIrreflexive()); // TRUE OK
-        System.out.println(g.isSymmetric()); // FALSE OK
-        System.out.println(g.isAntisymmetric()); // FALSE OK
-        System.out.println(g.isTransitive()); // FALSE OK
-        System.out.println(g.isAntiTransitive()); // FALSE OK
+    public void union(Graph g, Graph h) {
     }
 
-    // Fin MAIN
+    public void composition(Graph g, Graph h) {
+    }
+
+    public void subgraph(Graph g, Graph h) {
+
+    }
+
+    public java.lang.String toString() {
+        String ch = "";
+        int[] lst_sommet;
+        for (int i = 0; i < maximumNumberOfVertex; i++) {
+            if (hasVertex(i)) {
+                lst_sommet = getSuccessorList(i);
+                ch = ch + i + " :";
+                for (int j = 0; j < getVertexOutDegree(i); j++) {
+                    ch = ch + lst_sommet[j];
+                }
+                ch = ch + "\n";
+            }
+        }
+        return ch;
+    }
 
 }
-
-// Fin Classe Graphe
-
-/*
- * Notes Suppl�mentaires :
- * - Les Fonctions Union, Composition et Sousgraphe n'ont pas �t� compl�tement
- * termin�e et ne figurent pas dans le script ci-dessus.
- * - Tous les cas n'ont pas pu �tre trait� par manque de temps afin de v�rifier
- * correctement les fonctions de R�flexivit�, Sym�trie, Transitivit�.
- */
--- src/test/java/com/n5sstudio/GraphTest.java
+++ src/test/java/com/n5sstudio/GraphTest.java
@@ -1,5 +1,110 @@
-package test.java.com.n5sstudio;
+package com.n5sstudio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class GraphTest {
+
+    private Graph g;
+
+    @BeforeEach
+    public void setup() {
+        g = new Graph(5);
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addArc(1, 2, 5);
+    }
+
+    @Test
+    public void testHasVertex() {
+        assertTrue(g.hasVertex(1));
+        assertFalse(g.hasVertex(3));
+    }
+
+    @Test
+    public void testVertexCount() {
+        assertEquals(2, g.getVertexCount());
+    }
+
+    @Test
+    public void testAddVertex() {
+        g.addVertex(3);
+        assertTrue(g.hasVertex(3));
+        assertEquals(3, g.getVertexCount());
+    }
+
+    @Test
+    public void testDeleteVertex() {
+        g.deleteVertex(2);
+        assertFalse(g.hasVertex(2));
+        assertEquals(1, g.getVertexCount());
+    }
+
+    @Test
+    public void testHasArc() {
+        assertTrue(g.hasArc(1, 2));
+        g.addVertex(3);
+        assertFalse(g.hasArc(1, 3));
+    }
+
+    @Test
+    public void testGetArcValue() {
+        assertEquals(5, g.getArcValue(1, 2));
+        g.addVertex(3);
+        assertEquals(g.getUndefiledValue(), g.getArcValue(1, 3));
+        g.deleteVertex(2);
+        assertEquals(g.getUndefiledValue(), g.getArcValue(1, 2));
+    }
+
+    @Test
+    public void testGetVertexOutDegree() {
+        assertEquals(1, g.getVertexOutDegree(1));
+        assertEquals(0, g.getVertexOutDegree(2));
+    }
+
+    @Test
+    public void testGetVertexInDegree() {
+        assertEquals(0, g.getVertexInDegree(1));
+        assertEquals(1, g.getVertexInDegree(2));
+    }
+
+    @Test
+    public void testGetVertexDegree() {
+        assertEquals(1, g.getVertexDegree(1));
+        assertEquals(1, g.getVertexDegree(2));
+    }
+
+    @Test
+    public void testIsReflexive() {
+        
+    }
+
+    @Test
+    public void testIsIrreflexive() {
+        
+    }
+
+    @Test
+    public void testIsSymetric() {
+        
+    }
+
+    @Test
+    public void testIsAntiSymetric() {
+        
+    }
+
+    @Test
+    public void testIsTransitive() {
+        
+    }
+
+    @Test
+    public void testIsAntiTransitive() {
+        
+    }
 
 }

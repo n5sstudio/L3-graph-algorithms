fab951b2c4cf8b03120639474e7f92e93d5cb2d7
n5sstudio <n5sstudio@outlook.com>
2025-06-15 18:29:01 +0200

    fix: sonar

--- src/main/java/com/n5sstudio/Kruskal.java
+++ src/main/java/com/n5sstudio/Kruskal.java
@@ -7,23 +7,22 @@ import com.n5sstudio.exceptions.VertexOutboundLimitException;
 
 public class Kruskal {
 
-    private static Graph A;
+    private Graph tree;
     private Arc[] lstAreteTriee;
-    private Graph GraphG;
+    private Graph graph;
     private int poids;
-    public Stack[] C;
+    private Stack[] stacks;
 
     public Kruskal(int[][] mat) throws VertexAlreadyExistsException, VertexOutboundLimitException {
-        GraphG = new Graph(mat);
-        A = new Graph();
+        graph = new Graph(mat);
         for (int i = 0; i < 11; i++) {
-            if (GraphG.hasVertex(i)) {
-                A.addVertex(i);
+            if (graph.hasVertex(i)) {
+                tree.addVertex(i);
             }
         }
         poids = 0;
         lstAreteTriee = new Arc[55];
-        C = new Stack[GraphG.getMaximumNumberOfVertex()];
+        stacks = new Stack[graph.getMaximumNumberOfVertex()];
     }
 
     private Arc[] listeArc() throws VertexOutboundLimitException {
@@ -31,8 +30,8 @@ public class Kruskal {
         int k = 0;
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j <= i; j++) {
-                if (GraphG.hasArc(i, j)) {
-                    l[k] = new Arc(i, j, GraphG.getArcValue(i, j));
+                if (graph.hasArc(i, j)) {
+                    l[k] = new Arc(i, j, graph.getArcValue(i, j));
                     k++;
                 }
             }
@@ -54,63 +53,49 @@ public class Kruskal {
         return l;
     }
 
-    /* Returne l'arc d'indice i */
     private Arc aretesuivante(int i) {
         return lstAreteTriee[i];
     }
 
-    /* Algo de Kruskal */
-  public void fKruskal() throws VertexOutboundLimitException, ArcAlreadyExistsException, VertexDoesNotExistsException {
-    int indice = 0;
-    lstAreteTriee = trielstArete();
-    int k = 0;
-    for (int x = 0; x < 11; x++) {
-      C[x] = new Stack(GraphG.getMaximumNumberOfVertex());
-      C[x].push(x);
-    }
-    Arc suiv = aretesuivante(indice);
-    while (k < GraphG.getMaximumNumberOfVertex() - 1) {
-        A.addArc(suiv.getOrigin(), suiv.getDestination(), suiv.getValue());
-        for (int i = 0; i < GraphG.getMaximumNumberOfVertex() + 1; i++) {
-          if (C[suiv.getOrigin()].getSize() < GraphG.getMaximumNumberOfVertex() + 1) {
-            if (C[suiv.getDestination()].contains(C[suiv.getDestination()], i)) {
-                C[suiv.getOrigin()].push(i);
-            }
+    public void fKruskal()
+            throws VertexOutboundLimitException, ArcAlreadyExistsException, VertexDoesNotExistsException {
+        int indice = 0;
+        lstAreteTriee = trielstArete();
+        int k = 0;
+        for (int x = 0; x < 11; x++) {
+            stacks[x] = new Stack(graph.getMaximumNumberOfVertex());
+            stacks[x].push(x);
         }
-        for (int z = 0; z < 11; z++) {
-          if (C[suiv.getOrigin()].contains(C[suiv.getOrigin()], z)) {
-            for (int w = 0; w < GraphG.getMaximumNumberOfVertex() + 1; w++) {
-              if (C[suiv.getOrigin()].contains(C[suiv.getOrigin()], w)) {
-                C[suiv.getOrigin()] = new Stack(15);
-                C[z].push(w);
-              }
-            }
-          }
-        }
-        k = k + 1;
-      }
-      indice++;
-      suiv = aretesuivante(indice);
-    }
-  }
-
-    public void getArbre() throws VertexOutboundLimitException {
-        for (int i = 0; i < 11; i++) {
-            if (A.hasVertex(i)) {
-                System.out.print(i + ": ");
-                for (int j = 0; j < A.getVertexOutDegree(i); j++) {
-                    System.out.print(A.getSuccessorList(i)[j] + " ");
+        Arc suiv = aretesuivante(indice);
+        while (k < graph.getMaximumNumberOfVertex() - 1) {
+            tree.addArc(suiv.getOrigin(), suiv.getDestination(), suiv.getValue());
+            for (int i = 0; i < graph.getMaximumNumberOfVertex() + 1; i++) {
+                if (stacks[suiv.getOrigin()].getSize() < graph.getMaximumNumberOfVertex() + 1
+                        && stacks[suiv.getDestination()].contains(stacks[suiv.getDestination()], i)) {
+                    stacks[suiv.getOrigin()].push(i);
                 }
-                System.out.println(" ");
             }
+            for (int z = 0; z < 11; z++) {
+                if (stacks[suiv.getOrigin()].contains(stacks[suiv.getOrigin()], z)) {
+                    for (int w = 0; w < graph.getMaximumNumberOfVertex() + 1; w++) {
+                        if (stacks[suiv.getOrigin()].contains(stacks[suiv.getOrigin()], w)) {
+                            stacks[suiv.getOrigin()] = new Stack(15);
+                            stacks[z].push(w);
+                        }
+                    }
+                }
+            }
+            k = k + 1;
+            indice++;
+            suiv = aretesuivante(indice);
         }
     }
 
     public int getPoids() throws VertexOutboundLimitException {
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 12; j++) {
-                if (A.hasArc(i, j)) {
-                    poids = poids + GraphG.getArcValue(i, j);
+                if (tree.hasArc(i, j)) {
+                    poids = poids + graph.getArcValue(i, j);
                 }
             }
         }
--- src/main/java/com/n5sstudio/Prim.java
+++ src/main/java/com/n5sstudio/Prim.java
@@ -10,23 +10,21 @@ public class Prim {
     private int[][] lstAreteTriee;
     private Graph g;
     private int poids;
-    private int N;
+    private int maxNumberOfVertex;
 
     public Prim(int[][] mat) {
-        int t = 0;
         g = new Graph(mat);
-        N = mat.length;
+        maxNumberOfVertex = mat.length;
         poids = 0;
-        lstAreteTriee = new int[N * N][2];
+        lstAreteTriee = new int[maxNumberOfVertex * maxNumberOfVertex][2];
     }
-
-    /* Trie les Arc en fonction de leur valeur */
+    
     public void trieArete() throws VertexOutboundLimitException {
         int tmp1;
         int tmp2;
         int k1 = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < maxNumberOfVertex; i++) {
+            for (int j = 0; j < maxNumberOfVertex; j++) {
                 if (g.hasArc(i, j)) {
                     lstAreteTriee[k1][0] = i;
                     lstAreteTriee[k1][1] = j;
@@ -48,19 +46,17 @@ public class Prim {
             }
         }
     }
-    
-    private int[] choixAreteMin(Graph A) throws VertexOutboundLimitException {
+
+    private int[] choixAreteMin(Graph tree) throws VertexOutboundLimitException {
         int i = -1;
         boolean test = true;
         int[] tab = new int[2];
-        while (test == true) {
+        while (test) {
             i++;
-            if ((A.hasVertex(lstAreteTriee[i][0]) == true) &&
-                    (A.hasVertex(lstAreteTriee[i][1]) == false)) {
-                test = false;
-            }
-            if ((A.hasVertex(lstAreteTriee[i][0]) == false) &&
-                    (A.hasVertex(lstAreteTriee[i][1]) == true)) {
+            if ((tree.hasVertex(lstAreteTriee[i][0]) &&
+                    !tree.hasVertex(lstAreteTriee[i][1]))
+                    || (!tree.hasVertex(lstAreteTriee[i][0]) &&
+                            tree.hasVertex(lstAreteTriee[i][1]))) {
                 test = false;
             }
 
@@ -69,30 +65,29 @@ public class Prim {
         tab[1] = lstAreteTriee[i][1];
         return tab;
     }
-    
-    private Graph fPrim() throws VertexOutboundLimitException, VertexAlreadyExistsException, ArcAlreadyExistsException, VertexDoesNotExistsException {
+
+    public Graph fPrim() throws VertexOutboundLimitException, VertexAlreadyExistsException, ArcAlreadyExistsException,
+            VertexDoesNotExistsException {
         int x;
         int y;
-        int k = 0;
-        Graph A = new Graph();
+        Graph tree = new Graph();
         trieArete();
-        A.addVertex(5);
+        tree.addVertex(5);
 
-        while (A.getMaximumNumberOfVertex() < N) {
-            k++;
-            x = choixAreteMin(A)[0];
-            y = choixAreteMin(A)[1];
-            if (A.hasVertex(x)) {
-                A.addVertex(y);
+        while (tree.getVertexCount() < maxNumberOfVertex) {
+            x = choixAreteMin(tree)[0];
+            y = choixAreteMin(tree)[1];
+            if (tree.hasVertex(x)) {
+                tree.addVertex(y);
             } else {
-                A.addVertex(x);
+                tree.addVertex(x);
             }
-            A.addArc(x, y, g.getArcValue(x, y));
+            tree.addArc(x, y, g.getArcValue(x, y));
             poids = poids + g.getArcValue(x, y);
         }
-        return A;
+        return tree;
     }
-    
+
     public int getPoids() {
         return poids;
     }
